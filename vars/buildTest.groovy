9342a245754d819c4825eecb94e4bdbc9fd014e8
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-06-15 00:16:01 +0200

    Jenkins Shared Library v0.2

---
+++ vars/buildTest.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t zhajili/devops:jma-4.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push zhajili/devops:jma-4.0'
+    }
+}
