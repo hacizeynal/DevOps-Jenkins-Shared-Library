83619ada84b2190c4fd9d7f4d6ea0263e1bed337
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2022-06-17 16:46:11 +0200

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -15,7 +15,7 @@ def getCurrentBranch () {
 def call() {
     def branchName = getCurrentBranch()
     echo 'My branch is ' + branchName
-    echo "building the Jar File for ${env.BRANCH_NAME}"
+    "=================================="
     echo "building the Jar File for " + branchName
     sh "mvn package"
 }
