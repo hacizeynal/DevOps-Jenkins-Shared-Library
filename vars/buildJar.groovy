3bc1a48cb5fde40525b6901be00971f3153340b2
vugaragha <78844906+vugaragha@users.noreply.github.com>
2022-06-17 12:54:56 +0200

    Update buildJar.groovy
    Update the helper function to get the current branch name

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,15 +1,20 @@
 #!/usr/bin/env groovy
 
 def getCurrentBranch () {
-    return sh (
-        script: 'git rev-parse --abbrev-ref HEAD',
+    def fullRemoteBranchName =  sh (
+        script: 'git name-rev --name-only HEAD',
         returnStdout: true
     ).trim()
+    
+    echo "Full Remote Branch Name: " + fullRemoteBranchName
+    
+    def currentBranch = fullRemoteBranchName.substring(fullRemoteBranchName.lastIndexOf('/') + 1, fullRemoteBranchName.length())
+    return currentBranch
 }
 
 def call() {
     def branchName = getCurrentBranch()
-    echo 'My branch is' + branchName
+    echo 'My branch is ' + branchName
     echo "building the Jar File for ${env.BRANCH_NAME}"
     sh "mvn package"
 }
